f3611bce73fadfb5826099d0d1e8f5c35c6124b5
Yauhen.Malashchytski <Yauheni.Malashchytski@sephora.com>
2018-02-17 18:54:50 +0300

    small bugs fixed

--- api/analytics/report.py
+++ api/analytics/report.py
@@ -1,9 +1,8 @@
-def print_only_interesting(stocks, number_of_volume_spikes=0,
+def get_interesting_stocks(stocks, number_of_volume_spikes=0,
                            change_for_period=0.0,
                            max_day_range=0.0):
     interesting_stocks = []
     for s in stocks:
-        print str(s.vol_spikes_list[0])
         if s.vol_spikes_list[0] >= number_of_volume_spikes \
                 and s.max_day_range >= max_day_range:
             print s
--- api/main.py
+++ api/main.py
@@ -1,7 +1,7 @@
 import os
 from datetime import datetime
 
-from api.analytics.report import print_only_interesting, analyze
+from api.analytics.report import get_interesting_stocks, analyze
 from api.etl.transform.parsers import get_ticker_list
 from api.jobs.stock_list import StockList
 
@@ -12,19 +12,21 @@ def generate_reports(file_list, props, file_prefix):
         stock_list = StockList(tickers, props)
         # for s in stock_list.stocks:
         #     analyze(s)
-        interesting_stocks = print_only_interesting(stock_list.stocks, number_of_volume_spikes=3, max_day_range=0.2)
+        interesting_stocks = get_interesting_stocks(stock_list.stocks, number_of_volume_spikes=3, max_day_range=0.2)
         with open(f.replace(file_prefix, '..\\history\\').replace('.txt', '_' + datetime.now().strftime("%Y-%m-%d") + '.txt'), 'w') as out:
             for s in interesting_stocks:
                 out.write(str(s))
         for s in interesting_stocks:
             print s.stock_raw.symbol
 
+
 def test_report(tickers, props):
     stock_list = StockList(tickers, props)
     for s in stock_list.stocks:
         print s
-    interesting_stocks = print_only_interesting(stock_list.stocks, number_of_volume_spikes=1, max_day_range=0.2)
+    interesting_stocks = get_interesting_stocks(stock_list.stocks, number_of_volume_spikes=1, max_day_range=0.2)
     print interesting_stocks
+
 
 def read_properties(rel_file_path):
     props = {}
@@ -35,9 +37,15 @@ def read_properties(rel_file_path):
         props[k] = v.strip()
     return props
 
+
+def get_file_list(ticker_dir, skip_file=None):
+    file_list = [ticker_dir + l for l in os.listdir(ticker_dir) if l != skip_file]
+    return file_list
+
+
 if __name__ == '__main__':
-    prefix = '..\\tickers\\under_5\\'
-    file_list = [prefix + l for l in os.listdir(prefix)]
+    ticker_dir = '..\\tickers\\under_5\\'
+    file_list = get_file_list(ticker_dir, "basic_materials_under_5.txt")
     props = read_properties('..\\secrets\\credentials.properties')
-    generate_reports(file_list, props, prefix)
-    #test_report(['CC'], props)
+    generate_reports(file_list, props, ticker_dir)
+    # test_report(['CC'], props)
