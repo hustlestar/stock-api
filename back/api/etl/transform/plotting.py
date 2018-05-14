a1173cc7d4208e0a8d735e1e32c27af1949db1ab
Yauheni Malashchytski <yauheni_malashchytski@epam.com>
2018-05-14 15:15:32 -0400

    improved plotly layout code

--- back/api/etl/transform/plotting.py
+++ back/api/etl/transform/plotting.py
@@ -14,18 +14,20 @@ def plot_chart(stock_raw, save_to_dir):
         yaxis='y2')
     data = [trace]
 
-    layout = dict()
-    fig = dict(data=data, layout=layout)
-    fig['layout'] = dict()
-    fig['layout']['plot_bgcolor'] = 'rgb(250, 250, 250)'
-    fig['layout']['xaxis'] = dict(rangeselector=dict(visible=True))
-    fig['layout']['yaxis'] = dict(domain=[0, 0.2], showticklabels=False)
-    fig['layout']['yaxis2'] = dict(domain=[0.2, 0.8])
-    fig['layout']['legend'] = dict(orientation='h', y=0.9, x=0.3, yanchor='bottom')
-    fig['layout']['margin'] = dict(t=40, b=40, r=40, l=40)
+    data.append(dict(x=dates,
+                     y=[d.volume_ for d in stock_raw.daily_data],
+                     type='bar',
+                     yaxis='y',
+                     name='Volume'))
+    layout = graph_objs.Layout(
+        plot_bgcolor='rgb(250, 250, 250)',
+        xaxis=dict(rangeslider=dict(visible=False)),
+        yaxis=dict(domain=[0, 0.2], showticklabels=True),
+        yaxis2=dict(domain=[0.2, 1]),
+        legend=dict(orientation='h', y=0.9, x=0.3, yanchor='bottom'),
+        margin=dict(t=40, b=40, r=40, l=40)
+    )
 
-    fig['data'].append(dict(x=dates, y=[d.volume_ for d in stock_raw.daily_data],
-                            type='bar', yaxis='y', name='Volume'))
-    #plotly.offline.plot(fig, filename=save_to_dir + stock_raw.ticker + '.html', validate = False)
-    return plotly.offline.plot(fig, validate = False, output_type='div', auto_open=False)
-    # plotly.plotly.plot(data, filename='simple_candlestick')
+    figure = graph_objs.Figure(data=data, layout=layout)
+
+    return plotly.offline.plot(figure, validate=False, output_type='div', auto_open=False, show_link=False)
